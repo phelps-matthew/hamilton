7090974100f38b4a79bbbb794565556b56b509da
Matthew Phelps <matthewphelps@odysseyconsult.com>
2024-03-13 20:41:34 -1000

    add mount client/controller for testing new MessageNode

--- hamilton/devices/mount/client.py
+++ hamilton/devices/mount/client.py
@@ -1,24 +1,48 @@
-from hamilton.base.client import BaseClient
-from hamilton.devices.mount.config import Config
+from hamilton.base.message_node import MessageNode, MessageHandler
+from hamilton.devices.mount.config import MountClientConfig
+from hamilton.common.utils import CustomJSONDecoder
+import json
 
 
-class MountClient(BaseClient):
-    def __init__(self, config: Config):
-        super().__init__(config)
-        self.config = config
+class MountTelemetryHandler(MessageHandler):
+    def __init__(self):
+        super().__init__("telemetry")
+
+    def handle_message(self, ch, method, properties, body):
+        message = json.loads(body, cls=CustomJSONDecoder)
+        payload = message["payload"]
+        print(f"Telemetry Received. payload: {payload}")
+
+
+class MountClient:
+    def __init__(self, config, handlers: list[MessageHandler]):
+        self.node = MessageNode(config, handlers)
 
 
 if __name__ == "__main__":
-    client = MountClient(config=Config)
+    config = MountClientConfig()
+    handlers = [MountTelemetryHandler()]
+    client = MountClient(config, handlers)
+    try:
+        client.node.start()
+        import time
+        time.sleep(4)
+        command = "status"
+        parameters = {}
+        message = client.node.msg_generator.generate_command(command, parameters)
+        print("-" * 10)
+        print(message)
+        print("-" * 10)
+        response = client.node.publish_message("observatory.device.mount.command.status", message)
+        print("-" * 10)
+        print(response)
+        print("-" * 10)
+        response = client.node.publish_rpc_message("observatory.device.mount.command.status", message)
+        print("-" * 10)
+        print(f"response: {response}, response type: {type(response)}")
+        print("-" * 10)
 
-    command = "status"
-    parameters = {}
-    response = client.send_command(command, parameters)
-    print(response)
 
-    command = "set"
-    parameters = {"azimuth": float(270), "elevation": float(90)}
-    response = client.send_command(command, parameters)
-    print(response)
 
- 
+    finally:
+        client.node.stop()
--- hamilton/devices/mount/controller.py
+++ hamilton/devices/mount/controller.py
@@ -1,29 +1,53 @@
-from hamilton.base.controller import BaseController
-from hamilton.devices.mount.config import Config
+from hamilton.base.message_node import MessageNode, MessageHandler
 from hamilton.devices.mount.api import ROT2Prog
+from hamilton.devices.mount.config import MountControllerConfig
+from hamilton.common.utils import CustomJSONDecoder
+import threading
+import json
 
 
-class MountController(BaseController):
-    def __init__(self, config: Config, mount_driver: ROT2Prog):
-        super().__init__(config)
-        self.config = config
+class MountCommandHandler(MessageHandler):
+    def __init__(self, mount_driver: ROT2Prog):
+        super().__init__(message_type="command")
         self.mount = mount_driver
 
-    def process_command(self, command: str, parameters: str):
+    def handle_message(self, ch, method, properties, body):
         response = None
+        message = json.loads(body, cls=CustomJSONDecoder)
+        corr_id = properties.correlation_id
+        command = message["payload"]["commandType"]
+        parameters = message["payload"]["parameters"]
+
         if command == "set":
-            az = parameters.get("azimuth")
-            el = parameters.get("elevation")
-            response = self.mount.set(az, el)
+            response = self.mount.set(parameters.get("azimuth"), parameters.get("el"))
         elif command == "status":
             response = self.mount.status()
         elif command == "stop":
             response = self.mount.stop()
 
+        if response:
+            az, el = response
+            telemetry_msg = self.node_operations.msg_generator.generate_telemetry(
+                "azel", {"azimuth": az, "elevation": el}
+            )
+            self.node_operations.publish_message("observatory.device.mount.telemetry.azel", telemetry_msg, corr_id)
+
         return response
 
 
+class MountController:
+    def __init__(self, config, handlers: list[MessageHandler]):
+        self.node = MessageNode(config, handlers)
+
+
 if __name__ == "__main__":
-    mount_driver = ROT2Prog(Config.DEVICE_ADDRESS)
-    controller = MountController(config=Config, mount_driver=mount_driver)
-    controller.start()
+    config = MountControllerConfig()
+    mount_driver = ROT2Prog(config.DEVICE_ADDRESS)
+    handlers = [MountCommandHandler(mount_driver)]
+    controller = MountController(config, handlers)
+
+    controller.node.start()
+    import time
+    time.sleep(5)
+    # Blocks the main thread indefinitely until a SIGINT or SIGTERM is received.
+    # threading.Event().wait()
