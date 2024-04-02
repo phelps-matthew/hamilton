f411014955aa07ad16be17fafa61d74a0a362cc3
Matthew Phelps <matthewphelps@odysseyconsult.com>
2024-04-01 18:18:06 -1000

    add radiometrics

--- hamilton/log_collector/config.py
+++ hamilton/log_collector/config.py
@@ -9,6 +9,7 @@ class LogCollectorConfig(LogConfig):
         Exchange(name="database", type="topic", durable=True, auto_delete=False),
         Exchange(name="astrodynamics", type="topic", durable=True, auto_delete=False),
         Exchange(name="sdr", type="topic", durable=True, auto_delete=False),
+        Exchange(name="radiometrics", type="topic", durable=True, auto_delete=False),
     ]
     bindings = [
         Binding(exchange="mount", routing_keys=["#"]),
@@ -16,4 +17,5 @@ class LogCollectorConfig(LogConfig):
         Binding(exchange="database", routing_keys=["#"]),
         Binding(exchange="astrodynamics", routing_keys=["#"]),
         Binding(exchange="sdr", routing_keys=["#"]),
+        Binding(exchange="radiometrics", routing_keys=["#"]),
     ]
--- hamilton/radiometrics/controller.py
+++ hamilton/radiometrics/controller.py
@@ -1,27 +1,85 @@
-from hamilton.base.controller import BaseController
-from hamilton.radiometrics.config import Config
+import asyncio
+import signal
+from typing import Optional
+
+from hamilton.base.messages import Message, MessageHandlerType
 from hamilton.radiometrics.api import Radiometrics
+from hamilton.radiometrics.config import RadiometricsControllerConfig
+from hamilton.message_node.async_message_node_operator import AsyncMessageNodeOperator
+from hamilton.message_node.interfaces import MessageHandler
+from hamilton.database.client import DBClient
 
 
-class RadiometricsController(BaseController):
-    def __init__(self, config: Config, radiometrics: Radiometrics):
-        super().__init__(config)
-        self.config = config
-        self.radiometrics = radiometrics
+class RadiometricsCommandHandler(MessageHandler):
+    def __init__(self, radiometrics: Radiometrics, database: DBClient):
+        super().__init__(message_type=MessageHandlerType.COMMAND)
+        self.radiometrics: Radiometrics = radiometrics
+        self.db = database
+        self.startup_hooks = [self._start_db]
+        self.routing_key_base = "observatory.radiometrics.telemetry"
 
-    def process_command(self, command: str, parameters: str):
+    async def _start_db(self):
+        await self.db.start()
+
+    async def handle_message(self, message: Message, correlation_id: Optional[str] = None) -> None:
         response = None
+        telemetry_type = None
+        command = message["payload"]["commandType"]
+        parameters = message["payload"]["parameters"]
+
         if command == "get_tx_profile":
+            telemetry_type = "tx_profile"
             sat_id = parameters.get("sat_id")
-            response = self.radiometrics.get_tx_profile(sat_id)
+            response = await self.radiometrics.get_tx_profile(sat_id)
+
         elif command == "get_downlink_freqs":
             sat_id = parameters.get("sat_id")
-            response = self.radiometrics.get_downlink_freqs(sat_id)
+            telemetry_type = "downlink_freqs"
+            response = await self.radiometrics.get_downlink_freqs(sat_id)
 
-        return response
+        if telemetry_type is not None:
+            routing_key = f"{self.routing_key_base}.{telemetry_type}"
+            response = {} if response is None else response
+            telemetry_msg = self.node_operations.msg_generator.generate_telemetry(telemetry_type, response)
+            await self.node_operations.publish_message(routing_key, telemetry_msg, correlation_id)
+
+
+class RadiometricsController(AsyncMessageNodeOperator):
+    def __init__(self, config: RadiometricsControllerConfig = None):
+        if config is None:
+            config = RadiometricsControllerConfig()
+        self.db = DBClient()
+        radiometrics = Radiometrics(config=config, database=self.db)
+        handlers = [RadiometricsCommandHandler(radiometrics, self.db)]
+        super().__init__(config, handlers)
+
+
+shutdown_event = asyncio.Event()
+
+
+def signal_handler():
+    shutdown_event.set()
+
+
+async def main():
+    # Setup signal handlers for graceful shutdown
+    loop = asyncio.get_running_loop()
+    for signame in ("SIGINT", "SIGTERM"):
+        loop.add_signal_handler(getattr(signal, signame), signal_handler)
+
+    # Application setup
+    controller = RadiometricsController()
+
+    try:
+        await controller.start()
+        await shutdown_event.wait()  # Wait for the shutdown signal
+
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    finally:
+        await controller.stop()
 
 
 if __name__ == "__main__":
-    radiometrics = Radiometrics(config=Config)
-    controller = RadiometricsController(config=Config, radiometrics=radiometrics)
-    controller.start()
+    asyncio.run(main())
