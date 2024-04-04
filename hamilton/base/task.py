fd72dc63a76eab05b0b1f869b124813b3c716d02
Matthew Phelps <matthewphelps@odysseyconsult.com>
2024-04-03 18:06:04 -1000

    mv task generator

--- hamilton/base/task.py
+++ hamilton/base/task.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Union, Dict
+from typing import TypedDict, Union, Dict, Any
 from datetime import datetime, UTC
 from enum import Enum
 
@@ -9,11 +9,11 @@ class TaskType(Enum):
 
 class TaskParameters(TypedDict):
     sat_id: str
-    aos: str
-    los: str
-    tca: str
-    freq: str
-    interpolated_orbit: str
+    aos: dict[str, Any]
+    tca: dict[str, Any]
+    los: dict[str, Any]
+    sdr: dict[str, Any]
+    interpolated_orbit: dict[str, list[float, datetime]]
 
 
 class Task(TypedDict):
@@ -24,32 +24,17 @@ class Task(TypedDict):
     parameters: TaskParameters
 
 
-class MessageGenerator:
-    def __init__(self, source: str, version: str):
-        self.source = source
-        self.version = version
-
-    def _get_timestamp(self) -> str:
-        return datetime.now().isoformat()
-
-    def generate_message(self, message_type: MessageType, payload: Payload) -> Message:
-        message_schema: Message = {
-            "messageType": message_type,
-            "timestamp": self._get_timestamp(),
-            "source": self.source,
-            "version": self.version,
-            "payload": payload,
+class TaskGenerator:
+    @staticmethod
+    def generate_task(sat_id: str) -> Task:
+        task = {
+            "source": "hamilton",
+            "timestamp": datetime.now().isoformat(),
+            "task_id": str(uuid.uuid4()),
+            "task_type": "leo_track",
+            "parameters": None,
         }
-        return message_schema
 
-    def generate_command(self, command_type: str, parameters: Dict[str, Union[str, int, float]] = {}) -> Message:
-        payload: CommandPayload = {"commandType": command_type, "parameters": parameters}
-        return self.generate_message("command", payload)
 
-    def generate_telemetry(self, telemetry_type: str, parameters: Dict[str, Union[str, int, float]] = {}) -> Message:
-        payload: TelemetryPayload = {"telemetryType": telemetry_type, "parameters": parameters}
-        return self.generate_message("telemetry", payload)
 
-    def generate_response(self, response_type: str, data: Dict[str, Union[str, int, float]] = {}) -> Message:
-        payload: ResponsePayload = {"responseType": response_type, "data": data}
-        return self.generate_message("response", payload)
+
